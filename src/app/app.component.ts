b8e45986307a4addf7efddc5f08a2f96d6782047
Garreth <bgarreth@amazon.com>
2020-07-14 11:22:17 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import AWSAppSyncClient from 'aws-appsync/lib';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'testAppSync';
+
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+const appsyncClient = new AWSAppSyncClient({
+	 url: 'https://ylcdhpqfm5cqte6amtrjwijsxu.appsync-api.eu-central-1.amazonaws.com/graphql',
+  region: 'eu-central-1',
+  auth: {
+    type: 'API_KEY',
+    apiKey: 'da2-xjrbue3245htdhxzldftkwawc4',
+  },
+});
+  }
 }
